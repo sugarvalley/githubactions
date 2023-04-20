c585181ae4a7f103a7885d1a27eb81cd7989ff21
sugarvalley <cqrkowa@gmail.com>
2023-04-20 21:24:17 +0200

    notwork

--- src/test/java/GCDTest.java
+++ src/test/java/GCDTest.java
@@ -11,9 +11,9 @@ public class GCDTest {
     }
 
     @Test
-    public void testGCD2prime4() {
+    public void testGCD2prime3() {
         // Test 1: gcd of two prime numbersssss
-        assertEquals(2137, GCD.gcd(7, 11));
+        assertEquals(213223, GCD.gcd(7, 11));
     }
 
     @Test
