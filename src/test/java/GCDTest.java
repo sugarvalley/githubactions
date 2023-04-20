e8f176ef97edcab07297c6d4331374932ab1ef54
sugarvalley <cqrkowa@gmail.com>
2023-04-20 20:56:09 +0200

    siemmm

--- src/test/java/GCDTest.java
+++ src/test/java/GCDTest.java
@@ -12,7 +12,7 @@ public class GCDTest {
     @Test
     public void testGCD2prime3() {
         // Test 1: gcd of two prime numbersssss
-        assertEquals(1, GCD.gcd(7, 11));
+        assertEquals(3, GCD.gcd(7, 11));
     }
 
     @Test
